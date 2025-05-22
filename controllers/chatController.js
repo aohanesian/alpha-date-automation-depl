f052339c6dc8ddfc5142ec00b6e58cadcaf6e020
aohanesian <103946712+aohanesian@users.noreply.github.com>
2025-05-22 10:40:34 +0200

    7

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -9,9 +9,9 @@ router.get('/profiles', async (req, res) => {
     try {
         // const token = req.session.token;
         const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAzMzIsImVtYWlsIjoiT1AxNjgyMzUwNzQ3QGFscGhhLmRhdGUiLCJmaXJzdG5hbWUiOiLQktC40YLQsNC70LjRjyIsImxhc3RuYW1lIjoi0JPQsNCy0YDQuNC70LXQvdC60L4g0JTQldCd0KwiLCJhZ2VuY3lfaWQiOjc3MSwiZXh0ZXJuYWxfaWQiOjE2ODIzNTA3NDcsInRva2VuX2NyZWF0ZSI6IjIwMjUtMDUtMjJUMDg6MzU6MjguMjg1WiIsInRva2VuX2VuZCI6IjIwMjUtMDUtMjJUMTc6MzU6MjguMjg1WiIsImlhdCI6MTc0NzkwMjkyOCwiZXhwIjoxNzQ3OTM1MzI4fQ.jDASx41uW3v5wpZ0FDp0I-R6ODW1zBiR3LmOmy-CBAU'
-        if (!token) {
-            return res.status(401).json({ success: false, message: 'Not authenticated' });
-        }
+        // if (!token) {
+        //     return res.status(401).json({ success: false, message: 'Not authenticated' });
+        // }
 
         const profiles = await chatService.getProfiles(token);
         res.json({ success: true, profiles });
