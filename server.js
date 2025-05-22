b0ba8955dcb32b701c767c0b4d3a01d11d5d68da
aohanesian <103946712+aohanesian@users.noreply.github.com>
2025-05-22 08:42:58 +0200

    1

--- server.js
+++ server.js
@@ -49,4 +49,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-export default app;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
