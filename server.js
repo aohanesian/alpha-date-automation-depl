16dabcba52ab2a917cb3a291191c3b57bf0ad63a
aohanesian <103946712+aohanesian@users.noreply.github.com>
2025-05-22 08:55:02 +0200

    1

--- server.js
+++ server.js
@@ -42,4 +42,6 @@ app.use(express.static(path.join(__dirname, 'dist'), {
 }));
 
 // Start server
-export default app;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
